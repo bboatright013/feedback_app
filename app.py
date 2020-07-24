c7134bf2de52d869a01af0c5871231239df19afe
Brandon Boatright <bboatright013@gmail.com>
2020-07-24 11:20:52 -0400

    staged commit

---
+++ app.py
@@ -0,0 +1,17 @@
+"""Flask app for Feedback"""
+from flask import Flask, render_template, request, redirect, flash, session, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///feedback!!!'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+from flask_debugtoolbar import DebugToolbarExtension
+app.config['SECRET_KEY'] = "SECRET!"
+debug = DebugToolbarExtension(app)
+
+db.create_all()
